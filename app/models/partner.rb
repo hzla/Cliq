4436fd893c7ae87091dcc3deae7963cd1b4645b8
hzla00 <neohzla@gmail.com>
2014-07-15 14:01:11 -0700

    updated seed files

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,7 +1,7 @@
 class Partner < ActiveRecord::Base
 	has_many :events, dependent: :destroy 
 
-	attr_accessible :name, :description, :location, :start_time, :end_time, :location, :mon, :tues, :wed, :thurs, :fri, :sat, :sun
+	attr_accessible :name, :description, :location, :start_time, :end_time, :location, :mon, :tues, :wed, :thurs, :fri, :sat, :sun, :id
 
 	def self.local_options
 		self.all.map do |partner|
