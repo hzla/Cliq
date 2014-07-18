28555d08ac8c10326422e4105a573646db7ee97f
hzla00 <neohzla@gmail.com>
2014-07-17 19:22:45 -0700

    removed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 	has_many :messages, dependent: :destroy 
 
 	validates :name, :presence => true
-	attr_accessible :name, :email, :school, :bio, :profile_pic_url, :fb_token, :activation, :address, :sex, :sexual_preference, :latitude, :longitude, :active, :message_count, :invite_count, :event_count,:notify_messages, :notify_news, :notify_events, :timezone, :lbgtq, :role 
+	attr_accessible :name, :email, :school, :bio, :profile_pic_url, :fb_token, :activation, :address, :sex, :sexual_preference, :latitude, :longitude, :active, :message_count, :invite_count, :event_count,:notify_messages, :notify_news, :notify_events, :timezone, :lbgtq 
 
 	geocoded_by :address
 	after_validation :geocode      
