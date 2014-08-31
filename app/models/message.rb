ffc5aceb3b6a32813cbe0bae2e89657ef30f9134
hzla00 <neohzla@gmail.com>
2014-08-30 18:47:01 -0700

    fixed code for events display

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,21 @@ class Message < ActiveRecord::Base
 	def date user
 		(created_at + user.timezone.hours).strftime("%B %d")
 	end
+
+	def html_body
+		text = body.gsub(/(<.*>).*(<.*>)/, "")
+		text = text.gsub(/(<.*>)/, "")
+		other_text = text.gsub(/\[link\]\((.*)\)/, "")
+		match = Regexp.last_match(1)
+		if match
+			if match[0..6] != 'http://'
+				url_match = 'http://' + match
+			else
+				url_match = match
+			end
+			text.gsub(/\[link\]\((.*)\)/, "<a href='#{url_match}'>#{match}</a>")
+		else
+			text
+		end
+	end
 end
